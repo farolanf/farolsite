25640e4a7007745138257d5a930763ad99472fab
Farolan Faisal <farolanfaisal@gmail.com>
2022-09-08 00:41:44 +0000

    fix styles

--- windi.config.ts
+++ windi.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'windicss/helpers'
-import plugin from 'windicss/plugin'
 
 export default defineConfig({
+  theme: {
+    fontFamily: {
+      inconsolata: ['inconsolata'],
+    },
+  },
+  shortcuts: {
+    'container-ext': 'mx-auto px-15px lg:px-0',
+  },
   extract: {
     include: ['src/**/*.{js,ts,jsx,tsx,astro}'],
   },
