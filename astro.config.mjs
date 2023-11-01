97bb092c7bc8a610baca84ed20394ee41dc7ce5b
Farolan Faisal <farolanfaisal@gmail.com>
2023-11-01 10:06:05 +0800

    chore: deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [solidJs()],
   vite: {
     plugins: [VitePWA(), WindiCss()],
